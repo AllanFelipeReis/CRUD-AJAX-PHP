e4fe9f024646856307739fa8da1ef12f2ee3d657
AllanFelipeReis <allanreis3006@gmail.com>
2020-09-29 22:18:57 -0300

    Update

---
+++ api/config/people.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `pessoas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `pessoas`;
+
+CREATE TABLE `people` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `cpf` varchar(30) NOT NULL,
+  `birthDate` date NOT NULL,
+  `gender` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `people` (`id`, `name`, `cpf`, `birthDate`, `gender`) VALUES
+(NULL, 'Allan', '123.123.123-12', '2000-06-30', 'Masculino'),
+(NULL, 'Rafaela', '123.123.123-01', '1999-10-20', 'Feminino'),
+(NULL, 'Gabriel', '123.123.123-01', '1000-10-10', 'Masculino');
+
